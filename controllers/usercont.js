afaea612ed0c6ce7ef44c9c89904c8370a9d2f81
Nicholas Zentai <nicholasz8687@gmail.com>
2023-05-08 22:26:04 -0400

    finished userController

---
+++ controllers/usercont.js
@@ -0,0 +1,48 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers (req, res) {
+        User.find({})
+        .then(userData => res.json(userData));
+    },
+
+    getUserById (req, res) {
+        User.findById(req.params.userId)
+        .then(userData => res.json(userData));
+    },
+
+    createNewUser (req, res) {
+        User.create(req.body)
+        .then(userData => res.json(userData));
+    },
+
+    updateUserById (req, res) {
+        User.findOneAndUpdate(req.params.id, req.body, { new: true })
+        .then(userData => res.json(userData));
+    },
+
+    deleteUserById (req, res) {
+        User.findOneAndDelete(req.params.id)
+        .then(userData => res.json(userData));
+    },
+
+    addFriend (req, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId }},
+            { new: true }
+        ) .then(userData => res.json(userData));
+    },
+
+    deleteFriend (req, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        ) .then(userData => res.json(userData));
+    }
+};
+
+
+module.exports = userController;
+
