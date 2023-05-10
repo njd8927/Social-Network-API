10fc3eb8baf0ab3416ab81b3dcaf9f69b27f1366
Nicholas Zentai <nicholasz8687@gmail.com>
2023-05-09 21:11:01 -0400

    finished userroute

--- routes/api/userroute.js
+++ routes/api/userroute.js
@@ -1,1 +1,33 @@
 const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createNewUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/usercont');
+
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createNewUser);
+
+
+router
+    .route('/:userId')
+    .get(getUserById)
+    .put(updateUserById)
+    .delete(deleteUserById);
+
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+
+module.exports = router;
